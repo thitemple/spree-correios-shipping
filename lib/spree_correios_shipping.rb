ed25fbf88c3d538598356212019484fd66ceb64e
vintem <vintem@gmail.com>
2011-07-13 23:35:11 -0300

    removido versao antiga do sedex

--- lib/spree_correios_shipping.rb
+++ lib/spree_correios_shipping.rb
@@ -15,7 +15,7 @@ module SpreeCorreiosShipping
         Rails.env.production? ? require(c) : load(c)
       end
       
-      [SedexCalculator, Shipping::Sedex, Shipping::Sedex10, Shipping::SedexHoje, Shipping::Pac].each(&:register)
+      [Shipping::Sedex, Shipping::Sedex10, Shipping::SedexHoje, Shipping::Pac].each(&:register)
       
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
         Rails.env.production? ? require(c) : load(c)
